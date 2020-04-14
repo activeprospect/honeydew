057a48d710a5f7d1cbec3caf801db60c70b7b870
Samuel Mullen <samuel@samuelmullen.com>
2020-04-14 16:20:52 -0500

    Update Logger configs

--- examples/ecto_poll_queue/config/test.exs
+++ examples/ecto_poll_queue/config/test.exs
@@ -1,6 +1,6 @@
 use Mix.Config
 
-config :logger, compile_time_purge_level: :warn
+config :logger, compile_time_purge_matching: [[level_lower_than: :warn]]
 
 config :logger, :console, level: :warn
 
